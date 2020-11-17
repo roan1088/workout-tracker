1c05bfb055c85aac32cec44460893bfd54076c50
roan1088 <rohan_188@yahoo.com>
2020-11-17 17:28:54 -0500

    Added express server

---
+++ server.js
@@ -0,0 +1,22 @@
+// Importing express server module
+const express = require("express");
+const path = require("path");
+
+// Setting the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Setting the Express App to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// For static files, look in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// API Routes
+// HTML Routes
+
+// Starting the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
