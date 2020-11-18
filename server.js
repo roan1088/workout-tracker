0dcd0ffef4e612166ef02f613bb1e8b571063f04
roan1088 <rohan_188@yahoo.com>
2020-11-18 16:03:27 -0500

    Connected server to the db using mongoose

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 // Importing express server module
 const express = require("express");
 const path = require("path");
+// Importing the mongoose and morgan logger modules
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 // Setting the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Setting up logger
+app.use(logger("dev"));
+
 // Setting the Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connecting to the db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 // Routes
 // For static files, look in the public folder
