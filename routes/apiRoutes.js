9fafa450dd0a394658419e70413eaf1253f0f933
roan1088 <rohan_188@yahoo.com>
2020-11-18 18:40:52 -0500

    Added api routes to get and post workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,34 @@ const path = require("path");
 // Importing the models
 const db = require("../models");
 
+// GET all workouts
+Router.get("/workouts", (req, res) => {
+  db.Workout.find()
+  .then(workouts => {
+    res.json(workouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+// POST a new workout
+Router.post("/workouts", (req, res) => {
+  db.Workout.create(req.body)
+  .then(workout => {
+    res.json(workout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+// POST a new exercise and PUT it into a specific workout
+Router.put("/workouts/:id", (req, res) => {
+});
+
+// GET request for ??
+Router.get("/workouts/range", (req, res) => {
+});
+
 module.exports = Router;
